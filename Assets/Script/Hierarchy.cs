6d353ea7f8b8581638d1a1c50fd117029ddac84c
coco3167 <corentinboblet@gmail.com>
2022-06-30 02:34:28 +0200

    :memo: Hierarchy.cs Cellar done

--- Assets/Script/Hierarchy.cs
+++ Assets/Script/Hierarchy.cs
@@ -2,11 +2,18 @@
 
 Cellier -> "Vous êtes dans une pièce ressemblant à un cellier.\n\nDeux éléments vous sautent aux yeux :\n- Une grande et solide porte en bois.\n- Une imposante armoire semblant être faite du même bois."
     Ouvrir la Porte -> "Vous ne parvenez pas à l'ouvrir, elle semble fermée à clé.
-                    -< [récupérer clé] ""
+                    -< [récupérer clé] "Vous utilisez la clé trouvée dans l'armoire et, sans surprise, la porte s'ouvre sans problème vers une autre salle bien plus grande où vous vous dirigez sans plus attendre."[Direction Piece2]
 
     Ouvrir l'armoire -> "L'armoire  s'ouvre sans difficulté laissant échapper un grincement étrange étrangement satisfaisant."
-        Regarder dans l'armoire ->
-
+        Regarder dans l'armoire -> "En observant l'intérieur, vous remarquez l'ancienneté de l'endroit.\nL'ensemble est très poussiéreux et vous parvenez à peine à distinguer les différents éléments.
+            Observer les livres -> "Les livres sont décrépis et tombent presque entièrement en poussière quand vous essayez d'intéragir avec.\Le temps et les insectes ont bien fait leur travail."
+            Observer les vivres -> "Tout cela n'a plus aucun intérêt, même les boîtes de conserve sont dans un état de décomposition avancée."
+            Observer la peluche -> "La peluche de Mia, elle a bien souffert. Vous vous rendez compte que la peluche est aussi abimée que votre mémoire car vous ne savez pas qui est Mia."
+            Ouvrir le tiroit de gauche -> "Vous essayez de l'ouvrir mais quoi que vous tentiez le tiroir ne bouge pas d'un pouce.\nVous décidez de l'observer de plus proche et vous remarquez des symboles :\n\n<align=center>{Gestalt}<\align=center>\n\nVous décidez de ne pas y préter plus d'attention pour le moment et l'instant."
+            Ouvrir le tiroir de droite -> "Le tiroir s'ouvre difficilement en n'arrêtant pas de grincer (un peu d'huile ne serait pas de refus).\nA l'intérieur vous trouvez des feuilles protégées des intempéries et du passage du temps ainsi qu'une clée n'ayant elle pas été épargnée."
+                Lire les feuilles -> "{A écrire}"
+                Récupérer la clé -> "La clé est bien rouillée mais nul doute qu'elle fonctionnera, vous la récupérez."[Porte -<]
+            
 Piece 2
 
 */
